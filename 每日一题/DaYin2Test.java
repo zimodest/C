12c388496f2edcac0dd1008b8dba708902bb0c84
zimodest <44044154+zimodest@users.noreply.github.com>
2018-11-17 23:03:54 +0800

    内建锁11_17
     *2.编写一个程序，启动三个线程，三个线程的名称分别是 A，B，C；
     * 每个线程将自己的名称在屏幕上打印5遍，打印顺序是ABCABC...

---
+++ 每日一题/DaYin2Test.java
@@ -0,0 +1,20 @@
+package www.jin.javapratice;
+
+/**
+ *2.编写一个程序，启动三个线程，三个线程的名称分别是 A，B，C；
+ * 每个线程将自己的名称在屏幕上打印5遍，打印顺序是ABCABC...
+ * notifyAll();
+ *
+ *3.写3个线程，打印如下
+ * *Thread-0
+ * *Thread-0@Thread-1
+ * *Thread-0@Thread-1#Thread-2
+ * *Thread-0@Thread-1#Thread-2*Thread-0
+ * *Thread-0@Thread-1#Thread-2*Thread-0@Thread-1
+ * *Thread-0@Thread-1#Thread-2*Thread-0@Thread-1#Thread-2
+ * ...
+ * 重复30次
+ * StringBuffer ;
+ */
+public class DaYin2Test {
+}
