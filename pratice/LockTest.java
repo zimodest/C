98420c579f4bfd65f95585b2cbb969bcbd2c4007
zimodest <44044154+zimodest@users.noreply.github.com>
2018-11-17 23:06:11 +0800

    自实现lock锁11_17
    自定义lock锁

--- pratice/LockTest.java
+++ pratice/LockTest.java
@@ -1,37 +1,92 @@
-package www.jin.TongBu;
+package www.jin.lockTest;
 
-
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
-class MyLock implements Runnable{
-    private Lock lock = new ReentrantLock();
-    private int ticket = 500;
-    @Override
-    public void run() {
-        for(int i = 0; i<500;i++){
-            try {
-                lock.lock();
-                Thread.sleep(20);
-                if(ticket>0){
-                    System.out.println(Thread.currentThread().getName()+"还剩下"+this.ticket--+"票");
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();
+class Mutex implements Lock{
+
+    private Sync sync = new Sync();
+    static class Sync extends AbstractQueuedSynchronizer{
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if(arg != 1) {
+                throw new RuntimeException("信号量不为1");
             }
+            if(compareAndSetState(0,1)){
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
         }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            if(getState() == 0){
+               throw new IllegalMonitorStateException();
+
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+    }
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
     }
 }
 public class LockTest {
-    public static void main(String[] args) {
-        MyLock ml = new MyLock();
-        Thread threadA = new Thread(ml,"黄牛A");
-        Thread threadB = new Thread(ml,"黄牛B");
-        Thread threadC = new Thread(ml,"黄牛C");
-        threadA.start();
-        threadB.start();
-        threadC.start();
-    }
+    private static Mutex mutex = new Mutex();
+
+    public static void main(String[] args){
+        for (int i = 0; i < 10;i++) {
+            Thread thread = new Thread(() -> {
+
+                mutex.lock();
+                try {
+                    Thread.sleep(5000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    mutex.unlock();
+                }
+            });
+
+            thread.start();
+            }
+        }
 }
