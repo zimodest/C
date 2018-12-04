8ebc38fc706a51288cae5fd0dea0c7b4d7cb5582
zimodest <44044154+zimodest@users.noreply.github.com>
2018-12-04 21:55:50 +0800

    IO操作

---
+++ IO/内存流实现字母转换.java
@@ -0,0 +1,43 @@
+﻿package www.IO;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+
+import java.io.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FileTest {
+    public static void main(String[] args) throws Exception {
+	//内存流实现字母转换
+        String msg = "hello";
+
+        //取得内存流
+        ByteArrayInputStream input = new ByteArrayInputStream(msg.getBytes());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        int len = 0;
+        while((len = input.read()) != -1){
+            out.write(Character.toUpperCase(len));
+        }
+        //直接将内存流输出
+        System.out.println(out);
+
+    }
+
+    public static void Change(){
+        //内存流实现字母转换
+        String msg = "hello";
+
+        //取得内存流
+        ByteArrayInputStream input = new ByteArrayInputStream(msg.getBytes());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        int len = 0;
+        while((len = input.read()) != -1){
+            out.write(Character.toUpperCase(len));
+        }
+        //直接将内存流输出
+        System.out.println(out);
+    }
+    }
+
