6b3008215413401cb87d727ffaff228daf55f0d6
zimodest <44044154+zimodest@users.noreply.github.com>
2018-12-06 23:15:06 +0800

    12_6

---
+++ 加.java
@@ -0,0 +1,20 @@
+public class Add{
+
+public static void main(String []args){
+    int a=1;
+    int b=2;
+    int c=0;
+    c = a;
+    a = b;
+    b = c;
+    System.out.println(a,b)
+    
+  
+    
+    
+    
+    
+}
+
+
+}
