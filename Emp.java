ec9606b01beb8a0548adea193bca3a798ae047cf
zimodest <44044154+zimodest@users.noreply.github.com>
2018-12-07 22:46:24 +0800

    12_7

---
+++ Emp.java
@@ -0,0 +1,30 @@
+package Java_reflect;
+
+public class Emp {
+    private String name;
+    private String job;
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "name='" + name + '\'' +
+                ", job='" + job + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
